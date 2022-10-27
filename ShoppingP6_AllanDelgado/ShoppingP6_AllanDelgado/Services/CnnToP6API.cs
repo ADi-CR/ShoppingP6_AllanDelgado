348e32fa5697e2a7f6a81b64dd3ff92452ca383c
Allan Delgado Chavarría <allan_del@hotmail.com>
2022-10-26 22:21:13 -0600

    Semana 8

---
+++ ShoppingP6_AllanDelgado/ShoppingP6_AllanDelgado/Services/CnnToP6API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingP6_AllanDelgado.Services
+{
+    public static class CnnToP6API
+    {
+        //en esta clase se definen las rutas de consumo del API 
+        //además se define la info de API Key necesaria 
+        //para poder consumir los controladores. 
+
+        public static string ProductionURL = "http://192.168.0.169:45455/api/";
+        public static string TestingURL = "http://192.168.0.169:45455/api/";
+
+        public static string ApiKeyName = "P6ApiKey";
+        public static string ApiKeyValue = "P6shoppingQwerty1234/*";
+
+    }
+}
--- ShoppingP6_AllanDelgado/ShoppingP6_AllanDelgado/Views/UserSignUpPage.xaml.cs
+++ ShoppingP6_AllanDelgado/ShoppingP6_AllanDelgado/Views/UserSignUpPage.xaml.cs
@@ -6,20 +6,60 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ShoppingP6_AllanDelgado.ViewModels;
 
 namespace ShoppingP6_AllanDelgado.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UserSignUpPage : ContentPage
     {
+        UserViewModel viewModel;
+
         public UserSignUpPage()
         {
             InitializeComponent();
+
+            //se agrega en bindingContext de la vista contra el viewModel. 
+            BindingContext = viewModel = new UserViewModel();
+
+            LoadUserRolesList();
         }
+
+        private async void LoadUserRolesList()
+        {
+            PckUserRole.ItemsSource = await viewModel.GetUserRoleList();
+        }
+
 
         private async void BtnCancel_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
+        }
+
+        private async void BtnAdd_Clicked(object sender, EventArgs e)
+        {
+            //en este caso la llamada a la funcionalidad no será por Command
+            //TODO: implementar Command
+
+            bool R = await viewModel.AddNewUser(TxtName.Text.Trim(),
+                                                TxtEmail.Text.Trim(),
+                                                TxtPassword.Text.Trim(),
+                                                TxtEmailBackUp.Text.Trim(),
+                                                TxtPhone.Text.Trim());
+
+            if (R)
+            {
+                await DisplayAlert(":)", "Everything is OK", "OK");
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                await DisplayAlert(":(", "Something went wrong!", "OK");
+            }
+
+
+
+
         }
     }
 }
